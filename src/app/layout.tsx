db1afe77ffee417f03e55c19d851124979abf893
Neoznzoe <bessonvictor59@gmail.com>
2026-03-04 12:53:12 +0100

    feat(piwik): first install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,9 +86,28 @@ export default function RootLayout({
           strategy="afterInteractive"
           async
         />
+        <Script
+          id="CookieConsent"
+          src="https://policy.app.cookieinformation.com/uc.js"
+          data-culture="FR"
+          data-gcm-version="2.0"
+          strategy="beforeInteractive"
+        />
         <meta name="google-site-verification" content="OpBP-OpU253pvgSruj3o8hV4VM_c8I6i0EYVFtbt2F4" />
       </head>
       <body className="bg-white text-gray-900 antialiased min-h-screen flex flex-col">
+        <Script id="piwik-pro" strategy="afterInteractive">
+          {`
+            (function(window, document, dataLayerName, id) {
+              window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});
+              var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
+              var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName);
+              var qPString=qP.length>0?("?"+qP.join("&")):"";
+              tags.async=!0,tags.src="https://neoznzoe.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
+              !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
+            })(window, document, 'dataLayer', '63d0dcd9-adc4-4484-9a9b-5d278b1ca118');
+          `}
+        </Script>
         <JsonLd data={{ "@context": "https://schema.org", ...websiteJsonLd }} />
         <script
           type="application/ld+json"
